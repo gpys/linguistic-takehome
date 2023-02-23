91afe0d5d432a391c9d37e3855692a42620141ab
Jordan Jeter <jordan@Jordans-MacBook-Pro.local>
2023-02-23 02:55:22 -0500

    add unit test for saveDocument mutation

--- src/document/document.service.spec.ts
+++ src/document/document.service.spec.ts
@@ -3,19 +3,63 @@ import { DocumentService } from './document.service';
 import { PrismaService } from '../prisma/prisma.service';
 
 describe('DocumentService', () => {
-    let service: DocumentService;
+    let documentService: DocumentService;
+    let prismaService: PrismaService;
 
     beforeEach(async () => {
         const module: TestingModule = await Test.createTestingModule({
-            providers: [DocumentService, PrismaService],
+            providers: [DocumentService, {
+                provide: PrismaService,
+                useValue: {
+                    document: {
+                        create: jest.fn(),
+                        findMany: jest.fn(),
+                        findUniqueOrThrow: jest.fn(),
+                    }
+                }
+            }
+            ],
         }).compile();
 
-        service = module.get<DocumentService>(DocumentService);
-    })
-
-    it('should be defined', () => {
-        expect(service).toBeDefined();
+        documentService = module.get<DocumentService>(DocumentService);
+        prismaService = module.get<PrismaService>(PrismaService);
     });
 
+    it('should be defined', () => {
+        expect(documentService).toBeDefined();
+    });
+
+    describe('createDocument', () => {
+        it('should save new document with correct "title", "text", "authorID" fields', async () => {
+            const saveDocumentData = {
+                title: 'test document',
+                text: 'contents of test document',
+                authorID: 1,
+            };
+
+            await documentService.createDocument(saveDocumentData);
+
+            expect(prismaService.document.create).toHaveBeenCalledWith({
+                data: saveDocumentData,
+            });
+        });
+
+        it('should return saved document', async () => {
+            const saveDocumentData = {
+                title: 'test document',
+                text: 'contents of test document',
+                authorID: 1,
+            };
+
+            const savedDocument = {id: 1, ...saveDocumentData};
+
+            (prismaService.document.create as jest.Mock).mockResolvedValueOnce(savedDocument);
+
+            const result = await documentService.createDocument(saveDocumentData);
+
+            expect(result).toEqual(savedDocument);
+        })
+
+    })
 
 });

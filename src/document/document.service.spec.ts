f20bdd033656643ce9884dfbddaec036c452f0d6
Jordan Jeter <jordan@Jordans-MacBook-Pro.local>
2023-02-23 11:10:16 -0500

    add unit tests for document service get all and find unique

--- src/document/document.service.spec.ts
+++ src/document/document.service.spec.ts
@@ -63,5 +63,74 @@ describe('DocumentService', () => {
     })
 
     //todo write tests for other methods
+    describe('getAllDocuments', () => {
+        it('should call prismaService.document.findMany method with correct authorID', async () => {
+            const authorID = 1;
+
+            await documentService.getAllDocuments(authorID);
+
+            expect(prismaService.document.findMany).toHaveBeenCalledWith({
+                where: {
+                    authorID,
+                },
+            });
+
+        });
+
+        it('should return list of documents with correct authorID', async () => {
+            const authorID = 1;
+            const documents = [
+                { id: 1, title: 'Document 1', text: 'contents of Document 1', authorID },
+                { id: 2, title: 'Document 2', text: 'contents of Document 2', authorID }
+            ];
+
+            (prismaService.document.findMany as jest.Mock).mockResolvedValueOnce(documents);
+
+            const result = await documentService.getAllDocuments(authorID);
+
+            expect(result).toEqual(documents);
+        });
+    });
+
+    describe('getOneDocument', () => {
+        it('should call prismaService.document.findUniqueOrThrow method with correct document id', async () => {
+            const id = 1;
+
+            await documentService.getOneDocument(id);
+
+            expect(prismaService.document.findUniqueOrThrow).toHaveBeenCalledWith({
+                where: {
+                    id,
+                },
+            });
+        })
+
+        it('should return document with passed in id', async () => {
+            const id = 1;
+            const document = { id, title: 'document title', text: 'content of document', authorID: 1 };
+
+            (prismaService.document.findUniqueOrThrow as jest.Mock).mockResolvedValueOnce(document);
+
+            const result = await documentService.getOneDocument(id);
+
+            expect(result).toEqual(document);
+        });
+
+        it('should throw an error if no document with passed id is found', async () => {
+            const id = 1;
+            const error = new Error(`no document with id: ${id} could be found`);
+
+            (prismaService.document.findUniqueOrThrow as jest.Mock).mockRejectedValueOnce(error);
+
+            expect(documentService.getOneDocument(id)).rejects.toThrow(error);
+
+        });
+
+
+    });
+
+
+
+
 
 });

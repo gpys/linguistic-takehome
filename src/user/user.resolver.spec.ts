49457ffac55dba9ac9a63aa537c9dec7257123e5
Jordan Jeter <jordan@Jordans-MacBook-Pro.local>
2023-02-23 01:53:27 -0500

    get user unit tests to pass

--- src/user/user.resolver.spec.ts
+++ src/user/user.resolver.spec.ts
@@ -1,12 +1,15 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UserResolver } from './user.resolver';
+import { UserService } from './user.service';
+import { DocumentService } from '../document/document.service';
+import { PrismaService } from '../prisma/prisma.service';
 
 describe('UserResolver', () => {
   let resolver: UserResolver;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UserResolver],
+      providers: [UserService, UserResolver, DocumentService, PrismaService],
     }).compile();
 
     resolver = module.get<UserResolver>(UserResolver);
